1bfa345211bfa80df35bf1bd7d43402b9830d0a9
Shubam Dixit <shubamdixit31032007@gmail.com>
2026-05-21 20:04:02 -0800

    third

---
+++ Operator.java
@@ -0,0 +1,28 @@
+public class Operator {
+    
+public static void main(String[] args) {
+    // reational operator
+    //equal to
+
+    int a=10;
+    int b=20;
+    boolean c=(a==b);
+    System.out.println(c);
+    //not equal to
+    int x=4;
+    int y=10;
+    boolean z=(x!=y);
+    System.out.println(z);
+    //greater than equal to
+    int p=15;
+    int q=25;
+    boolean r=(p>=q);
+    System.out.println(r);
+    //less than equal to
+    int m=30;
+    int n=20;
+    boolean o=(m<=n);
+    System.out.println(o);
+
+    }
+}
