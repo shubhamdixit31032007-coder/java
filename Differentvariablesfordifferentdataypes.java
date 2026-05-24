cec65e3baa38f64ad8bad140a5c1103bc9ee4d6d
Shubam Dixit <shubamdixit31032007@gmail.com>
2026-05-23 19:09:08 -0800

    variable and datatypes

---
+++ Differentvariablesfordifferentdataypes.java
@@ -0,0 +1,23 @@
+public class Differentvariablesfordifferentdataypes {
+
+    public static void main(String[] args) {
+        int age=20;
+        long a=12345678L;
+        short num=83;
+        double pi=3.14;
+        float score=9.5f;
+        boolean pass=true;
+        char grade='A';
+        System.out.println("age:"+age);
+        System.out.println("a:"+a);
+        System.out.println("num:"+num);
+        System.out.println("pi:"+pi);
+        System.out.println("score:"+score);
+        System.out.println("pass:"+pass);
+        System.out.println("grade:"+grade);
+
+
+        
+    }
+    
+}
