bad2ab1bccb72a1adf7799af6683dd4c0dc021ed
Shubam Dixit <shubamdixit31032007@gmail.com>
2026-05-23 18:27:13 -0800

    triangle

---
+++ Areaoftriangle.java
@@ -0,0 +1,9 @@
+public class Areaoftriangle {
+    
+    public static void main(String[] args){
+        double base=10.0;
+        double height=5.0;
+        double area=(base*height)/2;
+        System.out.println("Area of Triangle: "+area);
+    }
+}
