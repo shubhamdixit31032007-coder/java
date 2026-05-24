3e66cf8f718d63f9d2f73a37866c6bd58e12be8a
Shubam Dixit <shubamdixit31032007@gmail.com>
2026-05-23 18:48:29 -0800

    maxscore

---
+++ Maxscore.java
@@ -0,0 +1,10 @@
+public class Maxscore {
+
+    public static void main(String [] args){
+        double maxscore=500.0;
+        double obtaindedscore=450.0;
+        double percentage=(obtaindedscore/maxscore)*100;
+        System.out.println("percentage: "+percentage+"%");
+    }
+    
+}
