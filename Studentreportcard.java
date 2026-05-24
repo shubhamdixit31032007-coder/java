6a274eb26b82df7dc192691a879a1502500a0874
Shubam Dixit <shubamdixit31032007@gmail.com>
2026-05-23 18:36:35 -0800

    report

---
+++ Studentreportcard.java
@@ -0,0 +1,15 @@
+public class Studentreportcard {
+
+    public static void main(String[] args){
+        String studentname="shubham";
+        int studentid=83;
+        double studentscore=9.5;
+        char studentgrade='A';
+        System.out.println("Student Name: "+studentname);
+        System.out.println("Student id: "+studentid);
+        System.out.println("Student Score: "+studentscore);
+        System.out.println("Student Grade: "+studentgrade);
+      
+    }
+    
+}
