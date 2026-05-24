90bd82169aa7c23b805a81071e4c77145dfe7bd0
Shubam Dixit <shubamdixit31032007@gmail.com>
2026-05-23 18:19:22 -0800

    sdetails

---
+++ Studentdetails.java
@@ -0,0 +1,22 @@
+public class Studentdetails {
+
+    public static void main(String[] args){
+        String studentname="shubham";
+        int studentusn=83;
+        int studentage=20;
+        String studentbranch="CSE";
+        char studentgrade='A';
+        double studentcgpa=9.5;
+        boolean studentpassed=true;
+        System.out.println("Student Name: "+studentname);
+        System.out.println("Student USN: "+studentusn);
+        System.out.println("Student Age: "+studentage);
+        System.out.println("Student Branch: "+studentbranch);
+        System.out.println("Student Grade: "+studentgrade);
+        System.out.println("Student CGPA: "+studentcgpa);
+        System.out.println("Student Passed: "+studentpassed);
+        
+
+    }
+    
+}
---
+++ Variables.java
@@ -0,0 +1,13 @@
+public class Variables {
+
+    public static void main(String[] args) {
+       int length=4;
+       int breath=6;
+       float area=length*breath;
+       System.out.println(area);
+    
+        
+          
+    }
+    
+}
